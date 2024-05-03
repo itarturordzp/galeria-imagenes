613a20e0612c05eed8a1f530eef1a63d2fbfa84b
itarturordzp <itarturordzp@gmial.com>
2024-05-03 15:38:22 -0600

    Creacion de archivos para el backend

--- server/index.js
+++ server/index.js
@@ -1,1 +1,16 @@
-// esto es una prueba para git
+import express from 'express'
+import fileUpload from 'express-fileupload'
+import indexRoutes from './routes/index'
+
+const app = express()
+
+app.set('port', process.env.PORT || 4000)
+
+app.use(fileUpload({
+    tempFileDir: '/temp'
+}))
+app.use(indexRoutes)
+
+
+app.listen(app.get('port'))
+console.log('server on port', app.get('port'))
---
+++ server/routes/images.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router()
+
+router.post('/api/images/upload',async(req,res)=>{})
+
+router.get('/api/images', async(req,res)=>{})
+
+router.get('/api/images/:id',async(req,res)=>{})
+
+router.delete('/api/images/:id',async(req,res)=>{})
+
+export default router
